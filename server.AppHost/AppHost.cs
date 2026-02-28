a2963edc0666cc6f53b7af4dbe128b896c03bd8a
adem <ceperadem@outlook.com>
2026-02-28 19:04:52 +0300

    review src dir

--- server.AppHost/AppHost.cs
+++ server.AppHost/AppHost.cs
@@ -3,15 +3,15 @@ var builder = DistributedApplication.CreateBuilder(args);
 builder.AddProject<Projects.Server_Api>("dotnet-api")
     .WithUrl("/swagger/", "Swagger" );
 
-builder.AddRustApp("actix-service", "../services/actix-web")
+builder.AddRustApp("actix-service", "../src/services/actix-web")
     .WithHttpEndpoint(port: 8080, env: "PORT")
     .WithUrl("/swagger/","Swagger");
 
-builder.AddGolangApp("go-fiber", "../services/go-fiber")
+builder.AddGolangApp("go-fiber", "../src/services/go-fiber")
     .WithHttpEndpoint(port: 8081, env: "PORT")
     .WithUrl("/swagger/", "Swagger" );
 
-builder.AddBunApp("nestjs-api", "../services/nestjs-api", "src/main.ts")
+builder.AddBunApp("nestjs-api", "../src/services/nestjs-api", "src/main.ts")
     .WithHttpEndpoint(port: 3000, env: "PORT")
     .WithUrl("/swagger", "Swagger" );
 
